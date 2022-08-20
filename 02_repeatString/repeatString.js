5d2cee0b3d92bd756f43f1e4f25cb5be95d98c88
foyah <yvessomba@gmail.com>
2022-08-20 03:13:43 +0200

    Second Exo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, times) {
+    let repeatedString = "";
+    while (times > 0) {
+      repeatedString += string;
+      times--;
+    }
+    return repeatedString;
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
