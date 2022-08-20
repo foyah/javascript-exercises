e8c872c7ac52266102c91f3f8c366bdb2bcf97a4
foyah <yvessomba@gmail.com>
2022-08-20 03:19:42 +0200

    Ajout commentaires

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -7,5 +7,8 @@ const repeatString = function(string, times) {
     return repeatedString;
   }
 
+  // Cela aide vraiment de d'effectuer une comparaison pour voir si le compteur est plus élevé et de décrémenter.
+  // Comme ça, la boucle part de plus haut et valide les tests.
+
 // Do not edit below this line
 module.exports = repeatString;
