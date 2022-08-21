7842727aa72c67751a95f897d4c3c9a1dc8e1964
foyah <yvessomba@gmail.com>
2022-08-21 22:30:22 +0200

    Commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(n,m) {
+    if (n<0 || m<0 || typeof(m) !== 'number' || typeof(n) !== 'number'){ // Vérification de l'input
+        return 'ERROR';
+
+    } else {
+
+        if(n<m){ //
+        n1 = n
+        n2 = m 
+        } else{
+
+            n1 = m
+            n2 = n
+        }
+
+        return (n1+n2)*(n2/2)}
+        }; {
 
 };
 
