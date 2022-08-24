ac6ef89011d0f2fb9006043d04682d4d6da89174
foyah <yvessomba@gmail.com>
2022-08-24 20:39:09 +0200

    Fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function fibonacci(nbr) {
+    if(nbr < 2){
+      return nbr;
+    }
+    return fibonacci(nbr - 1) + fibonacci(nbr - 2);
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
+
