6611c9cfc0e821aa4fd7a31cca50d7587d637460
foyah <yvessomba@gmail.com>
2022-08-24 20:20:28 +0200

    1er jet

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n < 0) return;
+  if (n < 2) return 1;
+  return n * factorial(n - 1);
+}
 
 // Do not edit below this line
 module.exports = {
