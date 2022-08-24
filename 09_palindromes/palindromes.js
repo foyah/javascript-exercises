ecb0a4d4ea26a11d7b5d5cf125e9eb1340b0c493
foyah <yvessomba@gmail.com>
2022-08-24 20:14:28 +0200

    Premier jet

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    var len = Math.floor(str.length / 2);
+    for (var i = 0; i < len; i++)
+      if (str[i] !== str[str.length - i - 1])
+        return false;
+    return true;
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
