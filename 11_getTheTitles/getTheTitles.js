bcc9f791d6ca48bba6785bbb2aed59fe9b9a5797
foyah <yvessomba@gmail.com>
2022-08-27 01:41:02 +0200

    Première tentative, bizarrement ça ne marche pas..

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+        let titles = [];
+        for(let i = 0; i < array.length ; i++){
+            array[i].title.push(titles);
+        }
+    }
+    // return titles
 
 // Do not edit below this line
 module.exports = getTheTitles;
