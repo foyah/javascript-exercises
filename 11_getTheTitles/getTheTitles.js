6658a5b3beac9f718bea7ed6700abd1c2028febb
foyah <yvessomba@gmail.com>
2022-08-28 19:14:35 +0200

    Ayé

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,6 @@
 const getTheTitles = function(array) {
-        let titles = [];
-        for(let i = 0; i < array.length ; i++){
-            array[i].title.push(titles);
-        }
-    }
+      return array.map(item => item.title)
+};
     // return titles
 
 // Do not edit below this line
